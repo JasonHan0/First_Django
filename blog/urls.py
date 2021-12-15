360cc6c85e4e4950d11feda38ae97f54875781df
K-JM418 <67450735+K-JM418@users.noreply.github.com>
2021-12-15 15:28:21 +0900

    오늘은 FBV, CBV로 페이지제작

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # <ip : port>/blog : 블로그 메인 페이지
     path('', views.index),
+    # <ip : port>/blog/글번호(pk)
+    # /blog/1234 -> 1234를 숫자가 아닌 문자열 '1234'로 인식
+    # path('<int:pk>/'),
+    path('<int:pk>/', views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -17,3 +17,15 @@ def index(request):
             'posts': posts
         }
     )
+# pk 같이 클라이언트가 서버에 보내는 데이터 : parameter
+# post, posts 등 서버가 클라이언트에 보내는 데이터 : attribute
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post' : post,
+        }
+    )
